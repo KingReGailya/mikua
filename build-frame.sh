fc38729d7141f49e68640ab3007fba5b7d53cb72
KingReGailya <gailregalia@gmail.com>
2022-08-09 16:22:39 -0400

    what this?

--- build-frame.sh
+++ build-frame.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
+# WARING MODIFFING THE FILE MAY END UP BRAKING THE PROGRAM 
+# Proceed with caiojsaobncs aiodhnas
+
 
 echo "what is name"
-read $name
+read name
 
 echo "what is image"
-read $image
+read image
 
 echo "what is link"
-read $link
+read link
 
 
-
-echo "mkdir $PWD/build-logs/$name/$date" >> $PWD/$name.build
-
-echo "sudo rm -r -v $PWD/builds/* >> $PWD/build-logs/$name/$date/'1-rm.log'" >> $PWD/$name.build
-
-echo "git clone $link >> $PWD/build-logs/$name/$date/'2-clone.log'" >> $PWD/$name.build
-
-echo "docker build -t $image $PWD/builds/YouTubeDownloader/. >> $PWD/build-logs/$name/$date/'3-build.log'" >> $PWD/$name.build
-
-echo "docker push $image >> $PWD/build-logs/$name/$date/'4-push.log'" >> $PWD/$name.build
-
+echo "mkdir $PWD/build-info/build-logs/$name" >> $PWD/build-info/build-list/$name'.build'
+echo "sudo rm -r -v $PWD/builds/*" ">"">" "$PWD/build-info/build-logs/name/""$""date""1-rm.log'" >> $PWD/build-info/build-list/$name'.build'
+echo "git clone $link build-repos/$name-git-repo" ">"">" "$PWD/build-info/build-logs/name/""$""date""'2-clone.log'" >> $PWD/build-info/build-list/$name'.build'
+echo "docker build -t $image $PWD/builds/$name/" ">"">" "$PWD/build-logs/$name/""$""date""/'3-build.log'" >> $PWD/build-info/build-list/$name'.build'
+echo "docker push $image" ">"">" "$PWD/build-logs/$name/""$""date""/'4-push.log'" >> $PWD/build-info/build-list/$name'.build'
 
