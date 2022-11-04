95b104808900dd566a396dcdf43c9f34d7b3be1c
KingReGailya <gailregalia@gmail.com>
2022-11-03 20:35:01 -0400

    dumb

--- install.sh
+++ install.sh
@@ -4,47 +4,40 @@ echo "Warning this program comes with no warrenty to any extent y / n ?"
 
 loc=`echo $PWD`
 
-
-
 function install(){
 
         sudo rm -r -v /bin/mikua
-
         sudo bash -c 'echo "#!/bin/bash" >> /bin/mikua'
         sudo bash -c 'echo loc='$loc' >> /bin/mikua'
-
         sudo bash -c 'echo cd '$loc/build-info/build-tools' >> /bin/mikua'
-        
         sudo bash -c 'sudo echo >> /bin/mikua'
-        
         sudo bash -c 'sudo echo "$PWD/build-info/build-tools/mikua" '$loc'/build-info/build-tools "$""*" >> /bin/mikua'
-
         sudo chmod u+x /bin/mikua
         sudo chmod 777 /bin/mikua 
-
-
+        sudo chmod u+x build-info/build-tools/*
 
 }
 
 function yesnt(){
         
         read answer     
-       
-        
+               
         case $answer in
                 "y" )
+                    
                     install ;;
-
              
                 "n" )
+                    
                     exit ;;
 
                 *  )
+                    
                     echo "y/n" 
                     yesnt ;;
 
         esac
-}
 
+}
 
 yesnt
