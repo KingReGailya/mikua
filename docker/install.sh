3dc91bc38287187dad2ea3c35bc73617c2339075
Gailya <gailregalia@gmail.com>
2023-05-06 21:50:38 -0400

    working

--- docker/install.sh
+++ docker/install.sh
@@ -6,15 +6,15 @@ loc=`echo $PWD`
 
 function install(){
 
-        sudo rm -r -v /bin/mikua
-        sudo bash -c 'echo "#!/bin/bash" >> /bin/mikua'
-        sudo bash -c 'echo loc='$loc' >> /bin/mikua'
-        sudo bash -c 'echo cd '$loc/build-info/build-tools' >> /bin/mikua'
-        sudo bash -c 'sudo echo >> /bin/mikua'
-        sudo bash -c 'sudo echo "$PWD/build-info/build-tools/mikua" '$loc'/build-info/build-tools "$""*" >> /bin/mikua'
-        sudo chmod u+x /bin/mikua
-        sudo chmod 777 /bin/mikua 
-        sudo chmod u+x build-info/build-tools/*
+        rm -r -v /bin/mikua
+        bash -c 'echo "#!/bin/bash" >> /bin/mikua'
+        bash -c 'echo loc='$loc' >> /bin/mikua'
+        bash -c 'echo cd '$loc/build-info/build-tools' >> /bin/mikua'
+        bash -c 'echo >> /bin/mikua'
+        bash -c 'echo "$PWD/build-info/build-tools/mikua" '$loc'/build-info/build-tools "$""*" >> /bin/mikua'
+        chmod u+x /bin/mikua
+        chmod 777 /bin/mikua 
+        chmod u+x build-info/build-tools/*
         echo "Os" >> $PWD/build-info/installed-version
         curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
         chmod a+rx /usr/local/bin/yt-dlp
